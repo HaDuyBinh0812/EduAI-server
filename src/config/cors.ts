3e16a04c7b2ac0d72e727fb3157d919087602563
Binh <haduybinh55555@gmail.com>
2025-07-17 01:57:45 +0700

    addCors

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,7 +1,7 @@
 import { CorsOptions } from "cors";
 
 export const corsOptions: CorsOptions = {
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://eduai-client.netlify.app"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type"],
